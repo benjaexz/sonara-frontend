45b8c270e2cf4115a1f657a636b608188f776c80
benjaexz <jacopimentelxd@gmail.com>
2026-09-13 11:35:00 -0400

    feat(auth): add authGuard and protect private routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { Register } from './pages/register/register';
 import { Favorites } from './pages/favorites/favorites';
 import { Playlists } from './pages/playlists/playlists';
 import { History } from './pages/history/history';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'login', component: Login },
     { path: 'register', component: Register },
-    { path: 'favorites', component: Favorites },
-    { path: 'playlists', component: Playlists },
-    { path: 'history', component: History }
+    { path: 'favorites', component: Favorites, canActivate: [authGuard] },
+    { path: 'playlists', component: Playlists, canActivate: [authGuard] },
+    { path: 'history', component: History, canActivate: [authGuard] }
 ];
